64e7ab78ccba071e538291a5c7364bf822ff828f
Tim Arendsen <t.j.m.arendsen@gmail.com>
2020-11-15 18:50:09 +0100

    Changed environment to HTTPS links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
 
-  API_URL: "http://games.arendz.nl/api",
-  APP_URL: "http://p.arendz.nl",
+  API_URL: "https://games.arendz.nl/api",
+  APP_URL: "https://p.arendz.nl",
   PUBNUB_PUBLISH_KEY: "pub-c-6d608833-76fc-45ed-b264-427a27d53711",
   PUBNUB_SUBSCRIBE_KEY: "sub-c-cfd0dea4-fca7-11ea-81c8-6616e216ad91",
   BARDS_MIN_PLAYER_COUNT: 2

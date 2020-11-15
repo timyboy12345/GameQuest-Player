b09598555a308fd8381fe34a911412e916fceaef
Tim Arendsen <t.j.m.arendsen@gmail.com>
2020-11-15 18:12:50 +0100

    Added production environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+
+  API_URL: "http://games.arendz.nl/api",
+  APP_URL: "http://p.arendz.nl",
+  PUBNUB_PUBLISH_KEY: "pub-c-6d608833-76fc-45ed-b264-427a27d53711",
+  PUBNUB_SUBSCRIBE_KEY: "sub-c-cfd0dea4-fca7-11ea-81c8-6616e216ad91",
+  BARDS_MIN_PLAYER_COUNT: 2
 };
